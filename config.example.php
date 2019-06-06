abc9efaed633251957b42e2adeba21c6fe0bd412
KashaVarya <vk@albedo-tech.com>
2019-06-06 13:32:10 +0300

    update config and composer.json

--- config.example.php
+++ config.example.php
@@ -2,12 +2,13 @@
 
 return [
     'database' => [
-        'name' => '',
-        'username' => '',
-        'password' => '',
+        'name' => 'your_dbname',
+        'username' => 'your_name',
+        'password' => 'your_pass',
         'connection' => 'mysql:host=localhost',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ],
-    ]
+    ],
+    'site_url' => 'https://www.kreuzwort-raetsel.net/uebersicht.html',
 ];
