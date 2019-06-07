1f4e8af555577390688b0ef89d43f65298fcf7c5
KashaVarya <vk@albedo-tech.com>
2019-06-07 12:37:31 +0300

    create database and tables

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -13,33 +13,40 @@ class QueryBuilder
 
     public function createDatabase($config)
     {
-        $statement = $this->pdo->prepare("CREATE DATABASE IF NOT EXISTS `{$config['name']}`;");
+        $statement = $this->pdo->prepare(
+            "CREATE DATABASE IF NOT EXISTS `{$config['name']}`;"
+        );
         $statement->execute();
     }
 
     public function createTables($config)
     {
         $statement = $this->pdo->prepare(
-            "CREATE TABLE `Questions`(
-             `id` INT NOT_NULL AUTO_INCREMENT PRIMARY KEY,
+            "USE `{$config['name']}`;"
+        );
+        $statement->execute();
+
+        $statement = $this->pdo->prepare(
+            "CREATE TABLE IF NOT EXISTS `Questions`(
+             `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
              `text` VARCHAR(455) NOT NULL DEFAULT ''
              );"
         );
         $statement->execute();
 
         $statement = $this->pdo->prepare(
-            "CREATE TABLE `Answers`(
-             `id` INT NOT_NULL AUTO_INCREMENT PRIMARY KEY,
+            "CREATE TABLE IF NOT EXISTS `Answers`(
+             `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
              `text` VARCHAR(455) NOT NULL DEFAULT '',
-             `symbols` INT NOT_NULL DEFAULT 1
+             `symbols` INT NOT NULL DEFAULT 0
              );"
         );
         $statement->execute();
 
         $statement = $this->pdo->prepare(
-            "CREATE TABLE `Questions_Answers`(
-             `question_id` INT NOT_NULL,
-             `answer_id` INT NOT_NULL,
+            "CREATE TABLE IF NOT EXISTS `Questions_Answers`(
+             `question_id` INT NOT NULL,
+             `answer_id` INT NOT NULL,
              PRIMARY KEY (`question_id`, `answer_id`),
              CONSTRAINT `FK_Questions` FOREIGN KEY (`question_id`)
                 REFERENCES `Questions` (`id`) ON DELETE CASCADE,
