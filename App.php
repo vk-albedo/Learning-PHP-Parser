bf4580d74c197a5831f4e54865e21c6b413679b2
KashaVarya <vk@albedo-tech.com>
2019-06-07 17:19:15 +0300

    add additional functions

--- App.php
+++ App.php
@@ -2,7 +2,12 @@
 
 namespace App;
 
+use Database\Connection;
+use Database\QueryBuilder;
+use DOMDocument;
+use DOMXPath;
 use Exception;
+use GuzzleHttp\Client;
 
 class App
 {
@@ -20,5 +25,53 @@ class App
         }
 
         return static::$registry[$key];
+    }
+
+    public static function get_xpath_from_page($url)
+    {
+        $client = new Client();
+        $request = $client->get($url);
+        $content = $request->getBody()->getContents();
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML($content);
+
+        return new DOMXPath($doc);
+    }
+
+    public static function add_set_to_redis($set, $key)
+    {
+        try {
+            $host = self::get('config')['host'];
+            $redis = self::get('redis');
+
+            $values = [];
+            foreach ($set as $item) {
+                $values[] = $host . $item->textContent;
+            }
+
+            $redis->sadd($key, $values);
+
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+        }
+    }
+
+    public static function push_to_db($question, $answers)
+    {
+        try {
+            $database = self::get('config')['database'];
+
+            $connection = new QueryBuilder(
+                Connection::make($database)
+            );
+
+            $connection->addElement($database, $question, $answers);
+
+            $connection = null;
+
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+        }
     }
 }
