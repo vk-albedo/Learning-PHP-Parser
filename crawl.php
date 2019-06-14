d657e5095405780504feb999081caf965958bb89
KashaVarya <vk@albedo-tech.com>
2019-06-14 12:35:34 +0300

    add logger

--- crawl.php
+++ crawl.php
@@ -3,10 +3,24 @@
 require 'vendor/autoload.php';
 
 use App\App;
+use Logging\Logging;
 
 App::bind('config', require 'config.php');
 
 Predis\Autoloader::register();
 
+$logger = new Logging();
+$logger->log(
+    'INFO',
+    'Crawler is running...',
+    __FILE__
+);
+
 $app = new App();
 $app->run();
+
+$logger->log(
+    'INFO',
+    'Crawler has finished',
+    __FILE__
+);
